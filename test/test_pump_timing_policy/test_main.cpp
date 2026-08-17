bd91ba6325ad58381d4521a2db6ce436802e2098
Andris Rožkalns <277435981+rozkalnsandris@users.noreply.github.com>
2026-08-17 19:05:15 +0200

    test: align pump elapsed policy coverage

--- test/test_pump_timing_policy/test_main.cpp
+++ test/test_pump_timing_policy/test_main.cpp
@@ -65,27 +65,24 @@ void test_huge_extension_cannot_overflow_past_hard_limit() {
   TEST_ASSERT_EQUAL_UINT32(179000U, result.addedMs);
 }
 
-void test_planned_duration_boundary_stops_exactly_on_time() {
-  TEST_ASSERT_TRUE(pump_timing_policy::shouldStopForElapsedTime(
+void test_duration_boundary_is_elapsed_exactly_on_time() {
+  TEST_ASSERT_TRUE(pump_timing_policy::hasElapsed(
       31000U,
       1000U,
-      30000U,
-      180000U));
+      30000U));
 }
 
-void test_before_planned_duration_does_not_stop() {
-  TEST_ASSERT_FALSE(pump_timing_policy::shouldStopForElapsedTime(
+void test_before_duration_boundary_is_not_elapsed() {
+  TEST_ASSERT_FALSE(pump_timing_policy::hasElapsed(
       30999U,
       1000U,
-      30000U,
-      180000U));
+      30000U));
 }
 
-void test_hard_limit_stops_even_if_planned_duration_is_larger() {
-  TEST_ASSERT_TRUE(pump_timing_policy::shouldStopForElapsedTime(
+void test_hard_limit_boundary_is_testable_independently() {
+  TEST_ASSERT_TRUE(pump_timing_policy::hasElapsed(
       181000U,
       1000U,
-      200000U,
       180000U));
 }
 
@@ -97,11 +94,10 @@ void test_elapsed_math_is_wrap_safe() {
       48U,
       pump_timing_policy::elapsedMs(now, start));
 
-  TEST_ASSERT_TRUE(pump_timing_policy::shouldStopForElapsedTime(
+  TEST_ASSERT_TRUE(pump_timing_policy::hasElapsed(
       now,
       start,
-      48U,
-      180000U));
+      48U));
 }
 
 int main() {
@@ -114,9 +110,9 @@ int main() {
   RUN_TEST(test_extension_is_capped_at_hard_limit);
   RUN_TEST(test_extension_at_hard_limit_adds_nothing);
   RUN_TEST(test_huge_extension_cannot_overflow_past_hard_limit);
-  RUN_TEST(test_planned_duration_boundary_stops_exactly_on_time);
-  RUN_TEST(test_before_planned_duration_does_not_stop);
-  RUN_TEST(test_hard_limit_stops_even_if_planned_duration_is_larger);
+  RUN_TEST(test_duration_boundary_is_elapsed_exactly_on_time);
+  RUN_TEST(test_before_duration_boundary_is_not_elapsed);
+  RUN_TEST(test_hard_limit_boundary_is_testable_independently);
   RUN_TEST(test_elapsed_math_is_wrap_safe);
   return UNITY_END();
 }
